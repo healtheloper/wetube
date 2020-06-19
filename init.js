f2910efc8cfbd8bae52f628cfd34676d71650613
healtheloper <58503584+healtheloper@users.noreply.github.com>
2020-06-19 21:29:14 +0900

    mvc

---
+++ init.js
@@ -0,0 +1,8 @@
+import app from "./app"
+
+const PORT = 4000;
+
+const handleListening = (req, res) =>
+    console.log(`✅ Listening on : http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
