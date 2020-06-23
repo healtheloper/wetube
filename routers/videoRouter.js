fcee65ae7bfb05cf7b8ebc2204be2034d128bd12
healtheloper <58503584+healtheloper@users.noreply.github.com>
2020-06-23 19:24:37 +0900

    create layout with pug

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
+import routes from "../routes";
+import {upload, home} from "../controllers/videoContoller"
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.home, home);
+videoRouter.get(routes.upload, upload);
 
 export default videoRouter;
